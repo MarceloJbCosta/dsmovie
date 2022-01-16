b24f252d28dd5ab69229bf6d3c643435bd3a3100
marcelojbcosta <marcelomogueco@gmail.com>
2022-01-16 10:32:29 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,7 +9,8 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    const [pageNumber] =  useState(0);
+
+    const [pageNumber, setPageNumber] =  useState(0);
 
 
     const [page, setPage] =  useState<MoviePage>({
@@ -36,9 +37,15 @@ function Listing() {
         });
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number)=>{
+        setPageNumber(newPageNumber);
+    }
+
+
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
